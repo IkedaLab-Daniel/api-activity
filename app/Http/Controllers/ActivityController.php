0a8ae4e29994d6af9d60b240f61e5044dd1a7c4a
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-08 01:59:57 +0800

    ActivityController - index

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Activity;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\Action;
+
+class ActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $query = Activity::query();
+
+        if ($request->has('status')){
+            $query->where('status', $request->status);
+        }
+
+        return response()->json($query->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
